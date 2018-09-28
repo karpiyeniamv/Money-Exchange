ae4b35d78e0035390e6b12ae74042ca42b42ffc9
Maryia Karpiyenia <karpiyenia.mv@gmail.com>
2018-09-28 15:56:36 +0300

    result

--- src/index.js
+++ src/index.js
@@ -2,4 +2,44 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
-}
+    if (currency >10000  )
+    {
+        return  {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+    if (currency < 0)
+    {
+        return [];
+    }
+    temp = currency;
+    var countH=tempF( 50);//50
+    var countQ=tempF( 25);//25
+    var countD=tempF( 10);//10
+    var countN=tempF( 5);//5
+    var countP=tempF( 1);//1
+
+    var myMap = {};
+    if (countH>0)
+        myMap['H'] =countH;
+    if (countQ>0)
+        myMap['Q'] =countQ;
+    if (countD>0)
+        myMap['D'] =countD;
+    if (countN>0)
+        myMap['N'] =countN;
+    if (countP>0)
+        myMap['P'] =countP;
+
+
+    function tempF (cost){
+        var count =0;
+        while (temp>cost-1)
+        {
+            count++;
+            temp-=cost;
+        }
+        return count;
+    }
+
+    return myMap;
+};
+
